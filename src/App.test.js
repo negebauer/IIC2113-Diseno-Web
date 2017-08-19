1fcb46406f6d1728e611d55df3df1c3d68fa57c8
negebauer <negebauer@uc.cl>
2017-08-19 17:14:54 -0300

    Rewrite test stuff

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,12 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import { shallow, mount } from "enzyme"
 import App from "./App"
-
-it("dom renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
-})
 
 it("shallow renders without crashing", () => {
   shallow(<App />)
 })
 
-it("mounts without crashing", () => {
+it("mount renders without crashing", () => {
   mount(<App />)
 })
 

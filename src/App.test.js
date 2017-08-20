2f960ed9c82c9200543a029dce70fa5e7ca9fbee
negebauer <negebauer@uc.cl>
2017-08-20 01:46:32 -0300

    Snap using enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { shallow, mount } from "enzyme"
+import toJson from "enzyme-to-json"
 import App from "./App"
 
 it("shallow renders without crashing", () => {
@@ -14,4 +15,9 @@ it("renders welcome message", () => {
   const wrapper = shallow(<App />)
   const welcome = <h2>Welcome to React</h2>
   expect(wrapper).toContainReact(welcome)
+})
+
+it("matches snapshot", () => {
+  const wrapper = mount(<App />)
+  expect(toJson(wrapper)).toMatchSnapshot()
 })

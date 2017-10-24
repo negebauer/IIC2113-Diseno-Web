1ced0d0a1a7569dd2bc49d978cfce73eca583686
negebauer <negebauer@uc.cl>
2017-10-23 22:34:24 -0300

    100% experiencies

--- src/screens/Experiences.test.js
+++ src/screens/Experiences.test.js
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import { shallow, mount } from 'enzyme'
 import toJson from 'enzyme-to-json'
+import _ from 'lodash/fp/object'
 import Experiences from './Experiences'
+import ExperienceTableItem from '../components/ExperienceTableItem'
 import configureStore from '../redux/store'
 
 let history
@@ -52,4 +54,44 @@ it('renders children', () => {
     </Provider>
   )
   expect(wrapper.children().exists()).toBeTruthy()
+})
+
+it('renders ExperienceTableItem', () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Experiences />
+      </ConnectedRouter>
+    </Provider>
+  )
+  store.replaceReducer(state =>
+    _.merge(state, {
+      user: {
+        experiences: [
+          {
+            id: 1,
+            name: 'Mi primera experiencia',
+            date: '2016-08-16T23:13:05.908Z',
+            description: 'En esta experiencia...',
+            users: [
+              {
+                id: 1,
+                name: 'Oscar Rios',
+                username: 'orrios',
+                created_at: '2016-08-16T23:13:05.908Z',
+              },
+              {
+                id: 2,
+                name: 'Nicolas Gebauer',
+                phone_number: 'negebauer',
+                created_at: '2016-09-16T23:13:15.908Z',
+              },
+            ],
+          },
+        ],
+      },
+    })
+  )
+  store.dispatch({ type: '' })
+  expect(wrapper.find(ExperienceTableItem).length).toBe(1)
 })
